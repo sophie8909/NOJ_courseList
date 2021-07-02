b40ed3cb6230eecb4c6111663566131f4597209e
sophie8909 <sophielien09@gmail.com>
2021-07-02 17:11:02 +0800

    copy vurtify.js theme from NOJ gitlab

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  // copy from gitlab
+  theme: {
+    themes: {
+      light: {
+        primary: '#003865',
+        secondary: '#93282C',
+        success: '#4EB844',
+        info: '#3584C9',
+        warning: '#E08200',
+        error: '#ff4343',
+      },
+    },
+  },
+
 });
